bc2d8f916caafb73dbbad2d84f099299e73b7628
MOHAMED-FAHIM06 <nadimrahman290@gmail.com>
2024-07-08 22:02:50 +0600

    Update npx17.js

--- scripts/commands/npx17.js
+++ scripts/commands/npx17.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf("🕵️‍♀️")==0 || body.indexOf("🤭")==0 || body.indexOf(" ")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf("🕵️‍♀️")==0 || body.indexOf("😽")==0 || body.indexOf(" ")==0) {
 		var msg = {
 				body: "ওই এসব কি করতেছ আমার লজ্জা লাগে না বুঝি 🙆🙈",
 				attachment: fs.createReadStream(__dirname + `/Nayan/lojja.mp3`)
